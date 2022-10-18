15278a3759c36fdb8463c51c560a1f7974208280
markopaunovic1 <106074769+markopaunovic1@users.noreply.github.com>
2022-10-18 20:54:47 +0200

    Add files via upload

---
+++ src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+
+    String name;
+
+    String wins;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWins() {
+        return wins;
+    }
+
+    public void setWins(String wins) {
+        this.wins = wins;
+    }
+}
