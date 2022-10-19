06d143f598cd6606f7c323ca1730e936728ec103
Roberto Potrus <34423774+robpot95@users.noreply.github.com>
2022-10-19 11:54:46 +0200

    Made som changes to the Counter

--- src/App.java
+++ src/App.java
@@ -1,11 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        /* boolean flag = true;
-        while (flag) {
-            new TimerDemo(30);
-            board.startGame();
-        }*/
     }
 }
