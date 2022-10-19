804b36da85afb9e2fc6e2f719c9c799f2e3645e6
Roberto Potrus <34423774+robpot95@users.noreply.github.com>
2022-10-19 10:05:58 +0200

    Added Position class and changes to Tile class

--- src/Tile.java
+++ src/Tile.java
@@ -1,67 +1,24 @@
+enum TileState {
+    NONE,
+    REVEALED,
+    MINE,
+    FLAGGED
+}
+
 public class Tile {
+    private TileState state;
+    private Position position;
 
-    public boolean revealedTile;  // This square has a bomb
-    public boolean flaggedTile;  // Marks this square with a flag
-    public boolean mineHereTile;  // Bomb is placed on this square
-    public int minesNearby; // Mines nearby
-
-    public int y;       // Y coordinate of this square
-    public int x;       // X coordinate of this square
-
-
-    public Tile() {
-        revealedTile = false;
-        flaggedTile = false;
-        mineHereTile = false;
-        minesNearby = 0;
-        y = 0;
-        x = 0;
+    public Tile(TileState state, Position position) {
+        this.state = state;
+        this.position = position;
     }
 
-    public Tile(int minesNearby) {
-        this.minesNearby = minesNearby;
+    public TileState getState() {
+        return state;
     }
 
-    public int getMinesNearby() {
-        return minesNearby;
+    public Position getPosition() {
+        return position;
     }
-
-    public void setMinesNearby(int minesNearby) {
-        this.minesNearby = minesNearby;
-    }
-
-    public boolean isRevealedTile() {
-        return revealedTile;
-    }
-
-    public void setRevealedTile(boolean revealedTile) {
-        this.revealedTile = revealedTile;
-    }
-
-    public boolean isFlaggedTile() {
-        return flaggedTile;
-    }
-
-    public void setFlaggedTile(boolean flaggedTile) {
-        this.flaggedTile = flaggedTile;
-    }
-
-    public boolean isMineHereTile() {
-        return mineHereTile;
-    }
-
-    public void setMineHereTile(boolean mineHereTile) {
-        this.mineHereTile = mineHereTile;
-    }
-
-    public int clear(){             // method to reveal the cell, returns 0 if there is a mine or number of mines nearby
-        this.revealedTile = true;
-        if(mineHereTile){
-        return 0;
-        }else
-        {
-            return minesNearby;
-        }
-
-        }
-        }
+}
