1b92aff0808803ba3bcfc4de5968a24039e7ee4c
MiguelMalungo <98886465+MiguelMalungo@users.noreply.github.com>
2022-10-19 09:44:58 +0200

    Timer Class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        /* boolean flag = true;
+        while (flag) {
+            new TimerDemo(30);
+            board.startGame();
+        }*/
     }
 }
